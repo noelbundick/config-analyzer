08fc7c8a287e95b28263ff69f827de703a85759a
Joshua Phelps <v-jophelps@microsoft.com>
2021-02-22 15:44:39 -0800

    rule definition interface added

---
+++ src/models/types.ts
@@ -0,0 +1,9 @@
+export interface RuleDefinition {
+    name: string;
+    description: string;
+    resourceGraph?: ResourceGraphQueryDefinition
+}
+
+export interface ResourceGraphQueryDefinition {
+    query: string;
+}
