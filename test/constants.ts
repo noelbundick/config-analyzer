23f01b53d29d755d2dac126836bcbc7e11a8ab68
Noel <noelbundick@gmail.com>
2021-02-23 09:20:56 -0800

    Initial resource graph client + test harness (#31)
    * Initial resource graph client + test harness
    
    * Only require subscriptionId when tests are enabled
    
    * fix env var handling

---
+++ test/constants.ts
@@ -0,0 +1,5 @@
+// Environment variables that are used by the test suite
+export const environment = {
+  subscriptionId: 'AZA_TEST_SUBSCRIPTION_ID',
+  runIntegrationTests: 'AZA_TEST_INTEGRATION',
+};
