a70979238a71aa0a30239ffaff82adb8c0bd6aa7
Joshua Phelps <v-jophelps@microsoft.com>
2021-02-23 15:19:12 -0800

    adding ResourceGraphRule

--- src/rules.ts
+++ src/rules.ts
@@ -1,3 +1,7 @@
+import { ResourcesResponse } from "@azure/arm-resourcegraph/esm/models";
+import { DefaultAzureCredential } from "@azure/identity";
+import { AzureClient } from "./azure"
+
 export interface Rule {
   name: string;
   description: string;
@@ -16,4 +20,37 @@ export interface ScanResult {
     ruleName: string;
     total: number;
     ids: string[];
+}
+
+interface QueryResponseColumn {
+    name: string;
+    type: string | object; 
+}
+
+export class ResourceGraphRule implements RuleExecutor {
+    name: string;
+    query: string;
+    subscriptionId: string;
+
+    constructor(name: string, queryObj: ResourceGraphQuery, subscriptionId: string){
+        this.name = name;
+        this.query = queryObj.query;
+        this.subscriptionId = subscriptionId;
+    }
+
+    execute(){
+        const credential = new DefaultAzureCredential();
+        const client = new AzureClient(credential);
+        const resources = client.queryResources(this.query, [this.subscriptionId]) 
+        return resources.then(r => this.toScanResult(r))
+    }
+
+    private toScanResult(response: ResourcesResponse){
+        const cols = response.data.columns as QueryResponseColumn[]
+        const rows = response.data.rows as []
+        const idIndex = cols.findIndex(c => c.name === 'id') 
+        const resourceIds = rows.map(r => r[idIndex]) 
+        const scanResult = {ruleName: this.name, total: response.totalRecords, ids: resourceIds}
+        return scanResult 
+    }
 }
