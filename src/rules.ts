a1bee0c1e80fb20b9097df398c23fa7e6a1e8389
Joshua Phelps <v-jophelps@microsoft.com>
2021-02-23 17:58:09 -0800

    lint fix + execute() is async

--- src/rules.ts
+++ src/rules.ts
@@ -1,57 +1,61 @@
-import { ResourcesResponse } from "@azure/arm-resourcegraph/esm/models";
-import { DefaultAzureCredential } from "@azure/identity";
-import { AzureClient } from "./azure"
+import {ResourcesResponse} from '@azure/arm-resourcegraph/esm/models';
+import {DefaultAzureCredential} from '@azure/identity';
+import {AzureClient} from './azure';
 
 export interface Rule {
   name: string;
   description: string;
-  resourceGraph?: ResourceGraphQuery
+  resourceGraph?: ResourceGraphQuery;
 }
 
 export type ResourceGraphQuery = {
-    type: 'resourceGraph'
-    query: string
-}
+  type: 'resourceGraph';
+  query: string;
+};
 
 export interface RuleExecutor {
   execute(): Promise<ScanResult>;
 }
 
 export interface ScanResult {
-    ruleName: string;
-    total: number;
-    ids: string[];
+  ruleName: string;
+  total: number;
+  ids: string[];
 }
 
 interface QueryResponseColumn {
-    name: string;
-    type: string | object; 
+  name: string;
+  type: string | object;
 }
 
 export class ResourceGraphRule implements RuleExecutor {
-    name: string;
-    query: string;
-    subscriptionId: string;
+  name: string;
+  query: string;
+  subscriptionId: string;
 
-    constructor(name: string, queryObj: ResourceGraphQuery, subscriptionId: string){
-        this.name = name;
-        this.query = queryObj.query;
-        this.subscriptionId = subscriptionId;
-    }
+  constructor(name: string, query: ResourceGraphQuery, subscriptionId: string) {
+    this.name = name;
+    this.query = query.query;
+    this.subscriptionId = subscriptionId;
+  }
 
-    execute(){
-        const credential = new DefaultAzureCredential();
-        const client = new AzureClient(credential);
-        const resources = client.queryResources(this.query, [this.subscriptionId]) 
-        return resources.then(r => this.toScanResult(r))
-    }
+  async execute() {
+    const credential = new DefaultAzureCredential();
+    const client = new AzureClient(credential);
+    const resources = client.queryResources(this.query, [this.subscriptionId]);
+    return await resources.then(r => this.toScanResult(r));
+  }
 
-    private toScanResult(response: ResourcesResponse){
-        const cols = response.data.columns as QueryResponseColumn[]
-        const rows = response.data.rows as []
-        const idIndex = cols.findIndex(c => c.name === 'id') 
-        const resourceIds = rows.map(r => r[idIndex]) 
-        const scanResult = {ruleName: this.name, total: response.totalRecords, ids: resourceIds}
-        return scanResult 
-    }
+  private toScanResult(response: ResourcesResponse) {
+    const cols = response.data.columns as QueryResponseColumn[];
+    const rows = response.data.rows as [];
+    const idIndex = cols.findIndex(c => c.name === 'id');
+    const resourceIds = rows.map(r => r[idIndex]);
+    const scanResult = {
+      ruleName: this.name,
+      total: response.totalRecords,
+      ids: resourceIds,
+    };
+    return scanResult;
+  }
 }
