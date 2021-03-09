d652bff3af7501fbda869ec2631206c5040cf091
Joshua Phelps <v-jophelps@microsoft.com>
2021-03-09 09:55:12 -0800

    updating test

--- test/unit/commands/scan.spec.ts
+++ test/unit/commands/scan.spec.ts
@@ -5,8 +5,14 @@ describe('scan unit tests', () => {
     .stdout({print: false})
     .command(['scan', '--dummy', '-v'])
     .it('runs scan --dummy', ctx => {
-      expect(ctx.stdout).to.contain('dummy-rule-1');
-      expect(ctx.stdout).to.contain('dummy-rule-2');
+      const rule1 =
+        '    dummy-rule-1\n        ✓ Mocks a multiple rule system\n\n';
+      const rule2 =
+        '    dummy-rule-2\n        ✓ Mocks a multiple rule system\n\n';
+      expect(ctx.stdout).to.contain(rule1);
+      expect(ctx.stdout).to.contain(rule2);
+      expect(ctx.stdout).to.contain('2 passing');
+      expect(ctx.stdout).to.contain('2 scanned');
     });
   test
     .stdout({print: false})
@@ -18,9 +24,12 @@ describe('scan unit tests', () => {
   test
     .stdout({print: false})
     .command(['scan', '-d', '-r', 'dummy-rule-2', '-v'])
-    .it('runs scan -d -r dummy-rule-2', ctx => {
+    .it('runs scan -d -r dummy-rule-2 -v', ctx => {
       expect(ctx.stdout).to.contain('dummy-rule-2');
       expect(ctx.stdout).to.not.contain('dummy-rule-1');
+      expect(ctx.stdout).to.contain('1 passing');
+      expect(ctx.stdout).to.contain('1 scanned');
+      expect(ctx.stdout).to.contain('✓ Mocks a multiple rule system');
     });
   test
     .stdout()
