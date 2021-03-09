bae21199b3313a7cc559494e105d1c1971cf4575
Joshua Phelps <v-jophelps@microsoft.com>
2021-03-09 09:29:25 -0800

    removing async

--- test/unit/commands/scan.spec.ts
+++ test/unit/commands/scan.spec.ts
@@ -4,21 +4,21 @@ describe('scan unit tests', () => {
   test
     .stdout({print: false})
     .command(['scan', '--dummy', '-v'])
-    .it('runs scan --dummy', async ctx => {
+    .it('runs scan --dummy', ctx => {
       expect(ctx.stdout).to.contain('dummy-rule-1');
       expect(ctx.stdout).to.contain('dummy-rule-2');
     });
   test
     .stdout({print: false})
     .command(['scan', '--dummy', '--rule', 'dummy-rule-1', '--verbose'])
-    .it('runs scan --dummy --rule dummy-rule-1', async ctx => {
+    .it('runs scan --dummy --rule dummy-rule-1', ctx => {
       expect(ctx.stdout).to.contain('dummy-rule-1');
       expect(ctx.stdout).to.not.contain('dummy-rule-2');
     });
   test
     .stdout({print: false})
     .command(['scan', '-d', '-r', 'dummy-rule-2', '-v'])
-    .it('runs scan -d -r dummy-rule-2', async ctx => {
+    .it('runs scan -d -r dummy-rule-2', ctx => {
       expect(ctx.stdout).to.contain('dummy-rule-2');
       expect(ctx.stdout).to.not.contain('dummy-rule-1');
     });
