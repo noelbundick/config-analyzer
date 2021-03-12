a18efb84073b6f2bc7c525d4bb2857ad8cb587aa
Joshua Phelps <v-jophelps@microsoft.com>
2021-03-12 12:41:09 -0800

    fix for mutating global test data

--- test/unit/rules.spec.ts
+++ test/unit/rules.spec.ts
@@ -4,18 +4,23 @@ import {ResourceGraphModels} from '@azure/arm-resourcegraph';
 import {HttpHeadersLike, WebResourceLike} from '@azure/ms-rest-js';
 
 describe('Resource Graph Rule', () => {
-  const mockResourcesResponse: ResourceGraphModels.ResourcesResponse = {
-    totalRecords: 1,
-    count: 1,
-    resultTruncated: 'false',
-    data: {columns: [{name: 'id', type: 'string'}], rows: [['mockResourceId']]},
-    _response: {
-      request: {} as WebResourceLike,
-      status: 200,
-      headers: {} as HttpHeadersLike,
-      bodyAsText: '',
-      parsedBody: {} as ResourceGraphModels.QueryResponse,
-    },
+  const mockResourcesResponse = (): ResourceGraphModels.ResourcesResponse => {
+    return {
+      totalRecords: 1,
+      count: 1,
+      resultTruncated: 'false',
+      data: {
+        columns: [{name: 'id', type: 'string'}],
+        rows: [['mockResourceId']],
+      },
+      _response: {
+        request: {} as WebResourceLike,
+        status: 200,
+        headers: {} as HttpHeadersLike,
+        bodyAsText: '',
+        parsedBody: {} as ResourceGraphModels.QueryResponse,
+      },
+    };
   };
   const rule = new ResourceGraphRule({
     name: 'test-rule',
@@ -23,8 +28,8 @@ describe('Resource Graph Rule', () => {
     description: 'Intentional bad query',
     type: RuleType.ResourceGraph,
   });
-  it('can produce a scan result', async () => {
-    const scanResult = rule.toScanResult(mockResourcesResponse);
+  it('can produce a scan result', () => {
+    const scanResult = rule.toScanResult(mockResourcesResponse());
     expect(scanResult).to.deep.equal({
       ruleName: rule.name,
       description: rule.description,
@@ -32,9 +37,10 @@ describe('Resource Graph Rule', () => {
       resourceIds: ['mockResourceId'],
     });
   });
-  it("should throw an errow if the 'id' column is not returned from Resource Graph", async () => {
-    mockResourcesResponse.data.columns = [];
-    const iThrowError = () => rule.toScanResult(mockResourcesResponse);
+  it("should throw an errow if the 'id' column is not returned from Resource Graph", () => {
+    const resourcesResponse = mockResourcesResponse();
+    resourcesResponse.data.columns = [];
+    const iThrowError = () => rule.toScanResult(resourcesResponse);
     expect(iThrowError).to.throw(
       Error,
       'Id column was not returned from Azure Resource Graph'
