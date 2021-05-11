33ee709da7c4e4c3e375f069e9ac75dcf4ea6096
Joshua Phelps <v-jophelps@microsoft.com>
2021-05-11 16:26:49 -0700

    removing global test rules

--- test/unit/scanner.spec.ts
+++ test/unit/scanner.spec.ts
@@ -1,11 +1,12 @@
 import {assert} from 'chai';
-import {getTestRules} from '..';
+import {Scanner} from '../../src/scanner';
 
 describe('Scanner', function () {
   this.slow(5000);
   this.timeout(8000);
   it('can load rules from a JSON file', async () => {
-    const rules = await getTestRules();
+    const scanner = new Scanner();
+    const rules = await scanner.getRulesFromFile('./test/rules.json');
     for (const r of rules) {
       assert.containsAllKeys(r, ['name', 'description', 'type']);
     }
